83a9e097df872b30c4cc00d53ea787b1b1df93a6
LuksikaSaksangawong <64278863+LuksikaSaksangawong@users.noreply.github.com>
2020-04-30 22:58:45 +0700

    โปรแกรมคำนวณเลข

---
+++ Exercise5 1_Luksika_S.py
@@ -0,0 +1,18 @@
+print("โปรแกรมคำนวณเลข")
+print("-----------------")
+x= int(input("x :"))
+y= int(input("y :"))
+print("------------------")
+print("x+y=คำตอบ1")
+print("x-y=คำตอบ2")
+print("x*y=คำตอบ3")
+print("x/y=คำตอบ4")
+print("------------------")
+Result1 = x+y
+Result2 = x-y
+Result3 = x*y
+Result4 = x/y
+print("คำตอบ1 =",Result1)
+print("คำตอบ2 =",Result2)
+print("คำตอบ3 =",Result3)
+print("คำตอบ4 =",Result4)
