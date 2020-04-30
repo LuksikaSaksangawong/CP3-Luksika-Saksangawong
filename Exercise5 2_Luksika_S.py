7a53e5bec3ac2cad886022cb0a1094e2aa249f3d
LuksikaSaksangawong <64278863+LuksikaSaksangawong@users.noreply.github.com>
2020-04-30 22:59:59 +0700

    โปรแกรมคำนวณระยะทาง

---
+++ Exercise5 2_Luksika_S.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวณหาระยะทาง")
+print("--------------------")
+v = int(input("v (Km)="))
+t = int(input("t (h)="))
+Result = v/t
+print("--------------------")
+print("ผลลัพธ์",Result,"Km/h")
